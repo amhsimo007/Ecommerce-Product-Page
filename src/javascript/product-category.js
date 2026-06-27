d29073d955e60fef00655809a509dfd106f5992b
amahzoune-mohamed <217381551+amhsimo007@users.noreply.github.com>
2026-06-27 22:06:50 +0100

    create logic for product category and hamburger-menu

--- src/javascript/product-category.js
+++ src/javascript/product-category.js
@@ -1,25 +1,27 @@
 // print product women
 const productWomen = document.querySelector("#women");
-const showProduct = document.querySelector("#show-product");
+const showAllProduct = document.querySelector("#show-all-product");
 const cardWoman = document.querySelector("#card-women");
 
 // Get Data from API
-async function getData() {
+async function getDataWoman() {
     try {
         const response = await fetch("http://localhost:3000/products");
-        const data = await response.json();
+        let data = await response.json();
+        data = data.filter(product => product.id === "product-f407b6c6-d0cd-4cc3-8082-9a25ddc91c09")
         printDataWoman(data);
     } catch (error) {
         console.log(error);
     }
 }
-getData();
+getDataWoman();
 
 function printDataWoman(data) {
     data.map(function (ele) {
-        productWomen.addEventListener("click", function () {
-            showProduct.style.display = "none";
-            cardWoman.style.display = "flex";
+        productWomen.addEventListener("click", () => {
+            cardWoman.classList.add("flex");
+            cardMan.classList.remove("flex");
+            showAllProduct.style.display = "none";
             cardWoman.innerHTML = `                
             <div class="gallery">
                 <img class="img-product-big" src="${ele.images[0]}" alt="picture-product">
@@ -57,4 +59,60 @@ function printDataWoman(data) {
 }
 
 // print product men
+const productMen = document.querySelector("#men");
+const cardMan = document.querySelector("#card-men");
 
+// Get Data from API
+async function getDataMen() {
+    try {
+        const response = await fetch("http://localhost:3000/products");
+        let data = await response.json();
+        data = data.filter(product => product.id === "product-i407b6c6-4kdl-2eec-7890-23f4b5c6d7e8")
+        printDataMan(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+getDataMen();
+
+function printDataMan(data) {
+    data.map(function (ele) {
+        productMen.addEventListener("click", () => {
+            cardMan.classList.add("flex");
+            cardWoman.classList.remove("flex");
+            showAllProduct.style.display = "none";
+            cardMan.innerHTML = `                
+            <div class="gallery">
+                <img class="img-product-big" src="${ele.images[0]}" alt="picture-product">
+                <div class="gallery-small">
+                    <img class="img-product-small" src="${ele.images[0]}" alt="picture-product">
+                    <img class="img-product-small" src="${ele.images[1]}" alt="picture-product">
+                    <img class="img-product-small" src="${ele.images[2]}" alt="picture-product">
+                    <img class="img-product-small" src="${ele.images[3]}" alt="picture-product">
+                </div>
+            </div>
+            <div class="description" id="show-data">
+                <span>${ele.brand}</span>
+                <h2 class="subtitle">${ele.name}</h2>
+                <p class="paragraph">${ele.description}</p>
+                <span class="price">$${ele.price}.00 <span class="percentage">${ele.reduction}%</span></span>
+                <s class="discount">$${ele.compare_at_price}</s>
+                <div class="counter-cart">
+                    <div class="counter">
+                        <span>-</span>
+                        <span>3</span>
+                        <span>+</span>
+                    </div>
+                    <div class="btn-action">
+                        <button id="add-to cart" class="add-to cart">add-to cart</button>
+                        <img
+                            class="cart"
+                            src="images/icon-cart.svg"
+                            alt="icon-cart" />
+                    </div>
+                </div>
+            </div>
+        `
+        })
+    })
+}
---
+++ src/javascript/responsive-navbar.js
@@ -0,0 +1,41 @@
+const hamburgerMenu = document.querySelector("#hamburger-menu");
+const menu = document.querySelector("#menu");
+const hamburgerIcon = document.querySelector("#icon-hamburger");
+const iconClose = document.querySelector("#icon-close");
+const header = document.querySelector("#header");
+const mediaQuery = window.matchMedia("(max-width: 1023px)");
+
+
+hamburgerIcon.addEventListener("click", function () {
+    menu.style.flexDirection = "column";
+    menu.style.gap = "15px";
+    menu.style.position = "absolute";
+    menu.style.zIndex = "1";
+    menu.style.top = "0";
+    menu.style.left = "0";
+    menu.style.backgroundColor = "white";
+    menu.style.width = "70%";
+    menu.style.height = "100%";
+    menu.style.padding = "40px 25px";
+    menu.style.borderRight = "2px solid var(--color-pri-orange)";
+    menu.style.borderBottom = "2px solid var(--color-pri-orange)";
+    menu.style.borderRadius = "5px";
+})
+
+hamburgerIcon.addEventListener("click", () => {
+    hamburgerIcon.classList.toggle("active");
+    menu.classList.toggle("active");
+    iconClose.classList.toggle("active");
+    header.classList.add("color-back");
+})
+
+iconClose.addEventListener("click", () => {
+    iconClose.classList.toggle("active");
+    menu.classList.toggle("active");
+    hamburgerIcon.classList.toggle("active");
+    header.classList.remove("color-back");
+})
+
+// if (mediaQuery.matches) {
+
+// }
