27148044a67eba9015029019b42da8f97e001c9f
amahzoune-mohamed <217381551+amhsimo007@users.noreply.github.com>
2026-07-15 08:39:13 +0100

    slider image and cart

--- src/javascript/cart.js
+++ src/javascript/cart.js
@@ -1,7 +1,15 @@
-// display All Products
+const cartIcon = document.querySelector("#cart");
+const displayCart = document.querySelector("#display-cart");
+const addToCartBtn = document.querySelectorAll(".add-to-cart");
 const showData = document.querySelector("#show-data");
 
-// Get Data from API
+cartIcon.addEventListener("click", function () {
+    displayCart.classList.toggle("flex");
+})
+
+let cart = [];
+let allProducts = [];
+
 async function getData() {
     try {
         const response = await fetch("http://localhost:3000/products");
@@ -12,11 +20,11 @@ async function getData() {
         console.log(error);
     }
 }
-getData();
+getData()
 
-function printData(products) {
+function printData(data) {
     showData.innerHTML = "";
-    products.map(function (ele) {
+    data.map(function (ele) {
         showData.innerHTML +=
             `
         <div class="content" id="content">
@@ -26,13 +34,13 @@ function printData(products) {
             <span class="price">$${ele.price}.00 <span class="percentage">${ele.reduction}%</span> <s class="discount">$${ele.compare_at_price}</s></span>
             </div>
             <div class="btn-action back-primary">
-                <button class="btn btn-xl primary add-to-cart" data-id="${ele.id}" onclick="addToCart(${ele.id})"><span>Bay Now</span></button>
+                <button class="btn btn-xl primary add-to-cart" data-id="${ele.id}" onclick="addToCart('${ele.id}')"><span>Bay Now</span></button>
                 <img
                     class="cart-white"
                     src="images/icon-cart.svg"
                     alt="icon-cart" />
             </div>
-            
+
         </div>
         `
         // Products for women will be displayed when you click on their image
@@ -81,7 +89,7 @@ function printData(products) {
                     <span><img class="icon-plus" id="icon-plus" src="images/icon-plus.svg" alt="icon-plus"></span>
                     </div>
                     <div class="btn-action back-primary d-flex">
-                        <button class="btn btn-xl secondary add-to-cart" data-id="${ele.id}">Add-to cart</button>
+                        <button class="btn btn-xl secondary add-to-cart" data-id="${ele.id}" onclick="addToCart('${ele.id}')">Add-to cart</button>
                         <img
                             class="cart-black"
                             src="images/icon-cart.svg"
@@ -90,6 +98,7 @@ function printData(products) {
                 </div>
             </div>
         `
+
                     // click on Thumbnails change image big change
                     const imagesProducts = [`${ele.images[0]}`, `${ele.images[1]}`, `${ele.images[2]}`, `${ele.images[3]}`];
                     const mainImageProduct = document.querySelector(".img-product-big");
@@ -105,7 +114,6 @@ function printData(products) {
                             this.classList.add("active");
                         });
                     });
-
                     // click in big image pop-up slider image It appears on the horizon
                     mainImageProduct.addEventListener("click", function () {
                         popUpGalleryProduct.style.display = "block";
@@ -173,10 +181,11 @@ function printData(products) {
                             popUpGalleryProduct.style.display = "none";
                         })
                     })
-                })
-            })
-        }
 
+                })
+            }
+            )
+        }
         // Products for men will be displayed when you click on their image
         let imgProductOne = document.querySelector("#product-i407b6c6-4kdl-2eec-7890-23f4b5c6d7e8");
 
@@ -224,7 +233,7 @@ function printData(products) {
                         <span>+</span>
                     </div>
                     <div class="btn-action back-primary d-flex">
-                        <button class="btn btn-xl secondary add-to-cart" data-id="${ele.id}">Add-to cart</button>
+                        <button class="btn btn-xl secondary add-to-cart" data-id="${ele.id}" onclick="addToCart('${ele.id}')">Add-to cart</button>
                         <img
                             class="cart-black"
                             src="images/icon-cart.svg"
@@ -270,8 +279,177 @@ function printData(products) {
                 })
             })
         }
+
     })
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function addToCart(id) {
+    const product = allProducts.find(item => item.id === id);
+    if (!product) return;
+    const existingProduct = cart.find(item => item.id === id);
+    if (!existingProduct) {
+        cart.push({
+            ...product,
+            quantity: 1
+        });
+    } else {
+        existingProduct.quantity++;
+    }
+    updateCart()
+}
+
+function increaseQuantity(id) {
+    const product = cart.find(item => item.id === id);
+    if (!product) return;
+    product.quantity++;
+    updateCart();
+}
+
+function decreaseQuantity(id) {
+    const product = cart.find(item => item.id === id);
+    if (!product) return;
+    if (product.quantity > 1) {
+        product.quantity--;
+    } else {
+        removeProduct(id);
+        return;
+    }
+    updateCart();
+}
+
+function removeProduct(id) {
+    cart = cart.filter(item => item.id !== id);
+    updateCart();
+}
+
+function saveCart() {
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+function loadCart() {
+    const data = localStorage.getItem("cart");
+    if (data) {
+        cart = JSON.parse(data);
+    }
+    renderCart();
+    updateBadge();
+}
+
+function updateCart() {
+    saveCart();
+    renderCart();
+    updateBadge();
+}
+
+function renderCart() {
+    displayCart.innerHTML = "";
+
+    if (getCartCount() === 0) {
+
+        displayCart.innerHTML =
+            `  
+        <h3>Cart</h3>
+        <p>Your cart is empty</p>
+        `
+    } else {
+        displayCart.innerHTML =
+            `
+     <h3>Cart</h3>
+    `
+        cart.forEach(item => {
+            displayCart.innerHTML += createCartItem(item);
+        });
+
+        displayCart.innerHTML +=
+            `
+        <span class="total-cart" id="total-cart">Total Cart: $${getCartTotal()}.00</span>    
+        <div class="btn-action back-primary d-flex">
+            <button id="checkout" class="btn btn-xl secondary checkout">Checkout</button>
+        </div>
+    `
+    }
+
+}
+
+function updateBadge() {
+
+    const badge = document.getElementById("cart-badge");
+
+    let count = 0;
+
+    count = getCartCount();
+
+    if (count === 0) {
+
+        badge.style.display = "none";
+
+    } else {
+
+        badge.style.display = "block";
+
+    }
+
+    badge.textContent = count;
+}
+
+function getCartCount() {
+
+    return cart.reduce((count, item) => {
+
+        return count + item.quantity;
+
+    }, 0);
+}
+
+function getCartTotal() {
+
+    return cart.reduce((total, item) => {
+
+        return total + getSubtotal(item);
+
+    }, 0);
+
+}
+
+function getSubtotal(item) {
+
+    return item.price * item.quantity;
+
+}
+
+function createCartItem(item) {
+    let total = 0;
+    const subtotal = item.price * item.quantity;
+    total += subtotal;
+    return `
+        <div class="cart-content">
+            <img class="cart-img" src="${item.images[0]}" alt="picture-${item.name}">
+            <div class="info-product">
+                <span>${item.name}</span>
+                <span>$${item.price}×${item.quantity} <p>$${getSubtotal(item)}.00</p></span>
+            </div>
+            <img class="icon-delete" id="icon-delete" src="images/icon-delete.svg" alt="icon-delete" />
+        </div>
+        `;
+}
+
+loadCart();
 
 // Navbar
 const hamburgerMenu = document.querySelector("#hamburger-menu");
